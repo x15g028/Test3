c0f19bd771a5b8202763e1c41489713362f20139
unknown <x15g028@PC505N027.chiba-fjb.ac.jp>
2015-09-11 11:51:51 +0900

    変更

--- src/Test01.java
+++ src/Test01.java
@@ -3,6 +3,9 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("ああああ");
+		System.out.println("いいいい");
+		System.out.println("すすすす");
 
 	}
 
